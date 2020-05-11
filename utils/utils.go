3bfb2b43d0808d6864d3cb342553464ef516a1c5
zhoupw@gmail.com <zhoupw@gmail.com>
2020-05-11 16:13:05 +0800

    refactor code

--- utils/utils.go
+++ utils/utils.go
@@ -20,6 +20,9 @@ package utils
 import (
 	"encoding/hex"
 	"fmt"
+	"strings"
+	"github.com/ontio/ontology/common/log"
+	"github.com/ontio/ontology/smartcontract/event"
 	rtypes "github.com/coinbase/rosetta-sdk-go/types"
 	"github.com/ontio/ontology-rosetta/config"
 	"github.com/ontio/ontology/common"
@@ -29,8 +32,6 @@ import (
 	"github.com/ontio/ontology/core/types"
 	"github.com/ontio/ontology/http/base/actor"
 	bcomn "github.com/ontio/ontology/http/base/common"
-	"github.com/ontio/ontology/smartcontract/event"
-	"strings"
 )
 
 var (
