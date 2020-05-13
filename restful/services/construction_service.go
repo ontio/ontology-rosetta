282d9836a3c318af59f9c51360f7c2ae4a0cf6f5
zhoupw@gmail.com <zhoupw@gmail.com>
2020-05-13 18:15:32 +0800

    add logs

--- restful/services/construction_service.go
+++ restful/services/construction_service.go
@@ -81,14 +81,16 @@ func (c ConstructionAPIService) ConstructionSubmit(
 
 	txbytes, err := common.HexToBytes(txStr)
 	if err != nil {
+		log.Errorf("[ConstructionSubmit]HexToBytes failed:%s",err.Error())
 		return nil, SIGNED_TX_INVALID
 	}
 	txn, err := ctypes.TransactionFromRawBytes(txbytes)
 	if err != nil {
+		log.Errorf("[ConstructionSubmit]TransactionFromRawBytes failed:%s",err.Error())
 		return nil, SIGNED_TX_INVALID
 	}
 	if errCode, desc := bcomn.SendTxToPool(txn); errCode != ontErrors.ErrNoError {
-		log.Debug("[ConstructionSubmit]SendTxToPool failed:%s", desc)
+		log.Errorf("[ConstructionSubmit]SendTxToPool failed:%s", desc)
 		return nil, COMMIT_TX_FAILED
 	}
 
